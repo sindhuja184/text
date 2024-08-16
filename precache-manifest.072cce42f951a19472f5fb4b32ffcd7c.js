56db56b042a4f9da07aeac75d09b72b45247a814
sindhuja184 <dapsindhuja@gmail.com>
2024-08-16 23:07:37 +0530

    Updates

---
+++ precache-manifest.072cce42f951a19472f5fb4b32ffcd7c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88397aab2ba6b0b8ab90abdceccf23d2",
+    "url": "/text/index.html"
+  },
+  {
+    "revision": "772edde771fd5098955a",
+    "url": "/text/static/css/2.b71989ea.chunk.css"
+  },
+  {
+    "revision": "d8f7dbb3e90c9f007011",
+    "url": "/text/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "772edde771fd5098955a",
+    "url": "/text/static/js/2.37df19bf.chunk.js"
+  },
+  {
+    "revision": "0eb3ca9d0061837f6c8f",
+    "url": "/text/static/js/3.31c88699.chunk.js"
+  },
+  {
+    "revision": "d8f7dbb3e90c9f007011",
+    "url": "/text/static/js/main.ae26f8da.chunk.js"
+  },
+  {
+    "revision": "7617f34c07ae51265f46",
+    "url": "/text/static/js/runtime~main.28b5cbcc.js"
+  }
+]);
